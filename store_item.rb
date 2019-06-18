2fda835d2c1ec355c8f501f852335c5f951fe5f5
Arzo Thakur <thakur.arzo@yahoo.comm>
2019-06-17 21:32:58 -0500

    store sell items

---
+++ store_item.rb
@@ -0,0 +1,24 @@
+# C.1 - You are the owner of a store that sells items (you decide what specifically). Each item has various properties such as color, price, etc.
+
+
+face_foundation = {
+  :brand => "Mac", 
+  :name => "Striboli", 
+  :shade => "Beige",
+  :number => "42"
+
+}
+
+
+# The foundation I use is from Mac which called Striboli, shade Beige and also here is the number 42, Try it.
+
+ p "The foundation I use is from #{face_foundation[:brand]} which is called #{face_foundation[:name]}, shade #{face_foundation[:shade]} and here is the number #{face_foundation[:shade]}, Try it." 
+
+#  face_foundation = {
+#   brand: "Mac", 
+#   name: "Striboli", 
+#   shade: "Beige",
+#   number: "42"
+
+# }
+
